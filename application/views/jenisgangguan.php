d518c87451a71980d32b8723619654a76f31da46
karinatasha <33344829+karinatasha@users.noreply.github.com>
2018-08-28 10:53:54 +0700

    kategori

--- application/views/jenisgangguan.php
+++ application/views/jenisgangguan.php
@@ -3,7 +3,7 @@
         <div class="container">
             <div class="row">
                     <div class="col-md-12">
-                        <h1 class="page-head-line">Jenis Gangguan</h1>
+                        <h1 class="page-head-line">Kategori Gangguan</h1>
                     </div>
                 </div>
                 <div class="row">
